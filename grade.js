3de0f0e7ed82279f35aec7bdb8d52d4bb260d48c
abdulrahman <abdulrahmanahmad_979@outlook.com>
2022-09-14 18:47:04 +0300

    done

---
+++ grade.js
@@ -0,0 +1,22 @@
+alert (" Welcome to my site, I'll be Calculating your grade")
+let grade = prompt ('enter your grade')
+console.log (grade)
+
+if (grade >= 90) {
+    console.log (" very very good")
+}
+ else if (  grade <= 89 & grade >= 80 ) {
+    console.log ("very good")
+} 
+else if ( grade <= 79 & grade >= 70) {
+    console.log ("good")
+} 
+else if ( grade <= 69 & grade >= 60) 
+{
+    console.log ( " fine")
+} else if ( grade <= 59 & grade >= 50) {
+    console.log ( " badd")
+} else if ( grade <= 50) {
+    console.log ("GAME OVER")
+}
+// javascript isn't java
